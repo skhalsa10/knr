d0088e1c0e4c516af346c90f09a882bb14b3dc55
Siri Khalsa <skhalsa10@live.com>
2016-09-02 11:16:16 -0600

    floating poit version

--- chapter1/c2fConverter.c
+++ chapter1/c2fConverter.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /* print Fahrenheit-Celsius table
-  for fahr = 0, 20, ..., 300 */
+  for fahr = 0, 20, ..., 300  gloating point version*/
 main()
 {
-  int fahr, celsius;
+  float fahr, celsius;
   int lower, upper, step;
 
   lower = 0; //lower limit of temp table
@@ -14,7 +14,7 @@ main()
   fahr = lower;
   while (fahr <= upper) {
     celsius = 5 * (fahr-32) / 9;
-    printf("%3d\t%3d\n", fahr, celsius );
+    printf("%3.0f\t%6.1f\n", fahr, celsius );
     fahr = fahr + step;
   }
 }
